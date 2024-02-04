0b9a85ccbaf0adae791ed3394dbb91241b9fa2e7
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-02-04 15:32:35 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,10 +1,17 @@
+const someModule = require('/workspace/Arrow-Functions/errow-function');
+
 /**
  * To run this file in Gitpod, use the 
  * command node arrow-functions.js in the terminal
  */
 
 // Vanilla JavaScript Function
-
+function addTwoNumbers(a, b) {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(2, 6);
+console.log(sum);
 // Arrow Function With Parameters
 
 // Single Line Arrow Function With Parameters
