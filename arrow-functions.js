71bc27696758404d3fd4800c6489993ab1f6810f
Noah Al-Samawi <noah.alsamawi@gmail.com>
2024-02-04 16:04:23 +0000

    Test Arrow ., single line arrow, implict  and returning mltiple lins functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,21 +1,35 @@
-const someModule = require('/workspace/Arrow-Functions/errow-function');
-
 /**
  * To run this file in Gitpod, use the 
  * command node arrow-functions.js in the terminal
  */
 
 // Vanilla JavaScript Function
-function addTwoNumbers(a, b) {
+//function addTwoNumbers(a, b) {
+    // Code block
+   // return a + b;
+//}
+//let sum = addTwoNumbers(2, 6);
+//console.log(sum);
+// Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
     // Code block
     return a + b;
 }
 let sum = addTwoNumbers(2, 6);
 console.log(sum);
-// Arrow Function With Parameters
-
 // Single Line Arrow Function With Parameters
-
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers(4, 6);
+console.log(sum2);
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello World!');
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    
+   "This is multiline string"
+
+)
+    console.log(returnMultipleLines());
+
